ad65cf329212ca827cfdcd8aff472e8c697ffad7
Zach Lowry <zach.lowry@scorm.com>
2015-06-16 10:18:20 -0500

    Update run.sh.

--- run.sh
+++ run.sh
@@ -1,29 +1,34 @@
-#!/bin/sh -e
+#!/bin/bash
 
-cd src
-npm update
-npm install
-
-export ENDPOINT=`curl -u $KEY:$SECRET dev.watershedlrs.com/api/organizations -XPOST -d'{"name": "Canformance Test ${BUILD_NUMBER}"}' -H "Content-Type: application/json" -i -s | grep Location: | sed 's/Location: //'`
+set -e
 
 function cleanup {
-    curl -u $KEY:$SECRET -H "Content-Type: application/json" -XDELETE $ENDPOINT
+    curl -u $KEY:$SECRET -H "Content-Type: application/json" -XDELETE $ORGANIZATION
 }
-
 
 trap cleanup EXIT
 
-(
+cd src
+npm install
+
+ORGANIZATION=`curl -u $KEY:$SECRET https://$HOST/api/organizations -XPOST -d"{\"name\": \"Canformance Test $BUILD_NUMBER\"}" -H "Content-Type: application/json" -i -s | grep Location: | sed 's/Location: //' | tr -d '\r'`
+if [ -z "$ORGANIZATION" ]; then
+    echo "couldn't create organization, failing."
+    exit -1
+fi
+
+export ENDPOINT="${ORGANIZATION}/lrs"
+echo $ENDPOINT
+
 set +e
-grunt jshint --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=jshint-test-results.xml
-grunt mochaTest:stage1-core --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-core-test-results.xml
-grunt mochaTest:stage1-adhocValid --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-adhocValid-test-results.xml
-grunt mochaTest:stage1-adhocInvalid --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-adhocInvalid-test-results.xml
-grunt primeLRS --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=primeLRS-test-results.xml
-grunt mochaTest:stage1-conflict --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-conflict-test-results.xml
-grunt updateConsistent --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=updateConsistent-test-results.xml
-grunt mochaTest:stage2-statementStructure --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-statementStructure-test-results.xml
-grunt retrieveConflictStatements --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=retrieveConflictStatements-test-results.xml
-grunt mochaTest:stage2-conflict --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-conflict-test-results.xml
-grunt mochaTest:stage2-streamQueries --endpoint=$ENDPOINT/lrs --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-streamQueries-test-results.xml
-)
+grunt jshint --endpoint=$ENDPOINT --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=jshint-test-results.xml
+grunt mochaTest:stage1-core --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-core-test-results.xml
+grunt mochaTest:stage1-adhocValid --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-adhocValid-test-results.xml
+grunt mochaTest:stage1-adhocInvalid --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-adhocInvalid-test-results.xml
+grunt primeLRS --xapi-version=1.0.0 --reporter=xunit --captureFile=primeLRS-test-results.xml
+grunt mochaTest:stage1-conflict --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-conflict-test-results.xml
+grunt updateConsistent --xapi-version=1.0.0 --reporter=xunit --captureFile=updateConsistent-test-results.xml
+grunt mochaTest:stage2-statementStructure --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-statementStructure-test-results.xml
+grunt retrieveConflictStatements --xapi-version=1.0.0 --reporter=xunit --captureFile=retrieveConflictStatements-test-results.xml
+grunt mochaTest:stage2-conflict --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-conflict-test-results.xml
+grunt mochaTest:stage2-streamQueries --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-streamQueries-test-results.xml
