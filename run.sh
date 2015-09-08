18b0e00eca4d5bdfdf881b2ad2cabc3e9251ff6a
Zach Lowry <zach.lowry@watershedlrs.com>
2015-09-08 11:58:34 -0500

    Update run script to increase timeout

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e
 
 function cleanup {
-    curl -u $KEY:$SECRET -H "Content-Type: application/json" -XDELETE $ORGANIZATION
+    curl -k -u $KEY:$SECRET -H "Content-Type: application/json" -XDELETE $ORGANIZATION
 }
 
 trap cleanup EXIT
@@ -11,7 +11,7 @@ trap cleanup EXIT
 cd src
 npm install
 
-ORGANIZATION=`curl -u $KEY:$SECRET http://$HOST/api/organizations -XPOST -d"{\"name\": \"Canformance Test $BUILD_NUMBER\"}" -H "Content-Type: application/json" -i -s | grep Location: | sed 's/Location: //' | tr -d '\r'`
+ORGANIZATION=`curl -k -vvv -u $KEY:$SECRET https://$HOST/api/organizations -XPOST -d"{\"name\": \"Canformance Test $BUILD_NUMBER\"}" -H "Content-Type: application/json" -i -s | grep Location: | sed 's/Location: //' | tr -d '\r'`
 if [ -z "$ORGANIZATION" ]; then
     echo "couldn't create organization, failing."
     exit -1
@@ -21,14 +21,14 @@ export ENDPOINT="${ORGANIZATION}/lrs"
 echo $ENDPOINT
 
 set +e
-grunt jshint --endpoint=$ENDPOINT --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=jshint-test-results.xml
-grunt mochaTest:stage1-core --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-core-test-results.xml
-grunt mochaTest:stage1-adhocValid --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-adhocValid-test-results.xml
-grunt mochaTest:stage1-adhocInvalid --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-adhocInvalid-test-results.xml
-grunt primeLRS --xapi-version=1.0.0 --reporter=xunit --captureFile=primeLRS-test-results.xml
-grunt mochaTest:stage1-conflict --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-conflict-test-results.xml
-grunt updateConsistent --xapi-version=1.0.0 --reporter=xunit --captureFile=updateConsistent-test-results.xml
-grunt mochaTest:stage2-statementStructure --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-statementStructure-test-results.xml
-grunt retrieveConflictStatements --xapi-version=1.0.0 --reporter=xunit --captureFile=retrieveConflictStatements-test-results.xml
-grunt mochaTest:stage2-conflict --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-conflict-test-results.xml
-grunt mochaTest:stage2-streamQueries --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-streamQueries-test-results.xml
+grunt jshint --endpoint=$ENDPOINT --username=$KEY --password=$SECRET --xapi-version=1.0.0 --reporter=xunit --captureFile=jshint-test-results.xml --timeout=20000
+grunt mochaTest:stage1-core --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-core-test-results.xml --timeout=20000
+grunt mochaTest:stage1-adhocValid --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-adhocValid-test-results.xml --timeout=20000
+grunt mochaTest:stage1-adhocInvalid --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-adhocInvalid-test-results.xml --timeout=20000
+grunt primeLRS --xapi-version=1.0.0 --reporter=xunit --captureFile=primeLRS-test-results.xml --timeout=20000
+grunt mochaTest:stage1-conflict --xapi-version=1.0.0 --reporter=xunit --captureFile=stage1-conflict-test-results.xml --timeout=20000
+grunt updateConsistent --xapi-version=1.0.0 --reporter=xunit --captureFile=updateConsistent-test-results.xml --timeout=20000
+grunt mochaTest:stage2-statementStructure --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-statementStructure-test-results.xml --timeout=20000
+grunt retrieveConflictStatements --xapi-version=1.0.0 --reporter=xunit --captureFile=retrieveConflictStatements-test-results.xml --timeout=20000
+grunt mochaTest:stage2-conflict --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-conflict-test-results.xml --timeout=20000
+grunt mochaTest:stage2-streamQueries --xapi-version=1.0.0 --reporter=xunit --captureFile=stage2-streamQueries-test-results.xml --timeout=20000
